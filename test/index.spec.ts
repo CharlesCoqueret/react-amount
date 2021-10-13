1725797b4c52beff60f8da743d184cc4ac649131
Charles Coqueret <charles.coqueret@gmail.com>
2021-10-13 08:50:30 +0200

    Remove unnecessary comment

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,7 +2,6 @@ import { Amount, ThousangGroupingStyle } from '../src/index';
 
 describe('index main export', () => {
   test('should export all references', async () => {
-    // Les méthodes publiques de `utils` sont maintenant des fonctions simulées
     expect(Amount).toBeTruthy();
     expect(ThousangGroupingStyle).toBeTruthy();
   });
